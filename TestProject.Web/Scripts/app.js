17e53327d23dd52ec2aad6fbfd77086c2bf0460f
Frylle Bancaso <akosifrylle@gmail.com>
2016-05-13 00:57:04 +0800

    Add test object

---
+++ TestProject.Web/Scripts/app.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular.module("myModule", []);
+
+app.controller("myController", function($scope) {
+    var employees = [
+        { FirstName: "Frylle", LastName: "Bancaso" },
+        { FirstName: "Paule", LastName: "See" }
+    ];
+
+    $scope.employees = employees;
+})
